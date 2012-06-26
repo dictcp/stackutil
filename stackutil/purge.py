ca4205204c81092b8c3401e80c1bf5c6da20dc25
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-06-26 11:33:17 -0400

    added PurgeInstances

--- stackutil/purge.py
+++ stackutil/purge.py
@@ -72,3 +72,39 @@ class PurgeIPS(NovaCommand):
                     'update fixed_ips set allocated=0,instance_id=NULL where id = %s', id)
             self.log.info('cleared address %s (id %s).' % (address, id))
 
+class PurgeInstances(NovaCommand):
+    '''Clear ip addresses from the Nova database.'''
+
+    def get_parser(self, *args, **kwargs):
+        p = super(PurgeInstances, self).get_parser(*args, **kwargs)
+        p.add_argument('--all', action='store_true')
+        return p
+
+    def take_action(self, args):
+        super(PurgeInstances, self).take_action(args)
+
+        if args.all:
+            res = self.engine.execute('''
+                select id, user_id, hostname, vm_state, task_state
+                    from instances''')
+        else:
+            res = self.engine.execute('''
+                select id, user_id, hostname, vm_state, task_state
+                    from instances
+                    where vm_state != 'active''')
+
+        rows = res.fetchall()
+        formatter = TableFormatter()
+        formatter.emit_list([
+            'id', 'user_id', 'hostname',
+            'vm state', 'task state',
+            ], rows, sys.stdout, args)
+
+        if args.dryrun:
+            return
+
+        for id, user_id, hostname, vm_state, task_state in rows:
+            res = self.engine.execute(
+                    'delete from fixed_ips where id = %s', id)
+            self.log.info('delete instance %s (id %s).' % (hostname, id))
+
